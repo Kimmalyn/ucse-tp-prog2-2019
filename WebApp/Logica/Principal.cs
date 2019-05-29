7b7428e6a89b75cd0cd92c81ddd5aed4a1966676
LUCAS\Lucas <lucasdanielcopes@gmail.com>
2019-05-29 19:36:38 -0300

    Definicion de capas

---
+++ WebApp/Logica/Principal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contratos;
+using Newtonsoft.Json;
+
+namespace Logica
+{
+    class Principal
+    {
+        
+        public Resultado AltaDirectora(Directora directora, UsuarioLogueado usuarioLogueado)
+        {
+            //id nombre apellido email institucion cargo fecha ingreso
+            var listadirectora = new List<Directora>();
+
+            if (!File.Exists(@"C:\Datos\Directoras.txt"))
+            {
+                File.Create(@"C:\Datos\Directoras.txt").Close();
+            }
+
+            using (StreamWriter writer = new StreamWriter(@"C:\Datos\Directoras.txt", false))
+            {
+                string jsonDirectoras = JsonConvert.SerializeObject(listadirectora);
+                writer.Write(jsonDirectoras);
+            }
+
+            throw new NotImplementedException();
+        }
+
+    }
+}
